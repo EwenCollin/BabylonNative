f780f57969f94853dd2172af5c08f886e10cf07d
EwenCollin <51873655+EwenCollin@users.noreply.github.com>
2024-08-08 10:39:27 +0200

    Update XR.cpp

--- Dependencies/xr/Source/ARCore/XR.cpp
+++ Dependencies/xr/Source/ARCore/XR.cpp
@@ -227,7 +227,10 @@ namespace xr
             void main() {
                 vec4 baseColor = texture(babylonTexture, babylonUV);
                 //baseColor.w = 1.0;
-                baseColor.w = unpackAlpha(baseColor.w) * DepthGetVisibility(depthTexture, babylonUV, unpackDepth(baseColor.w) * 64.0);
+                baseColor.g = unpackDepth(baseColor.w) * 32.0;
+                baseColor.r = 1.0;
+                baseColor.b = 1.0;
+                baseColor.w = unpackAlpha(baseColor.w) * DepthGetVisibility(depthTexture, babylonUV, unpackDepth(baseColor.w) * 64.0 * 1000.0);
                 oFragColor = baseColor;  //Depth texture visualization only (testing)
             }
         )"};
